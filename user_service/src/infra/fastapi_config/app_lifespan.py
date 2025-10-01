d7cb15d4d89b3264d629d48b421c228fe3d4a074
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-10-01 22:26:03 +0330

    push the code

--- user_service/src/infra/fastapi_config/app_lifespan.py
+++ user_service/src/infra/fastapi_config/app_lifespan.py
@@ -6,12 +6,12 @@ from .app_state import AppStates, set_app_state
 @asynccontextmanager
 async def lifespan(app: FastAPI):
         
-    set_app_state(app, AppStates.EXTERNAL_FASTAPI_PORT, settings.EXTERNAL_FASTAPI_PORT)
-    set_app_state(app, AppStates.INTERNAL_FASTAPI_PORT, settings.INTERNAL_FASTAPI_PORT)
+    # set_app_state(app, AppStates.EXTERNAL_FASTAPI_PORT, settings.EXTERNAL_FASTAPI_PORT)
+    # set_app_state(app, AppStates.INTERNAL_FASTAPI_PORT, settings.INTERNAL_FASTAPI_PORT)
     
-    set_app_state(app, AppStates.AUTH_BASE_URL, settings.AUTH_BASE_URL)
-    set_app_state(app, AppStates.PRODUCT_BASE_URL, settings.PRODUCT_BASE_URL)
-    set_app_state(app, AppStates.ORDER_BASE_URL, settings.ORDER_BASE_URL)
+    # set_app_state(app, AppStates.AUTH_BASE_URL, settings.AUTH_BASE_URL)
+    # set_app_state(app, AppStates.PRODUCT_BASE_URL, settings.PRODUCT_BASE_URL)
+    # set_app_state(app, AppStates.ORDER_BASE_URL, settings.ORDER_BASE_URL)
             
     yield
     
--- user_service/src/infra/settings/settings.py
+++ user_service/src/infra/settings/settings.py
@@ -2,12 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     
-    EXTERNAL_FASTAPI_PORT: int
-    INTERNAL_FASTAPI_PORT: int
+    # EXTERNAL_FASTAPI_PORT: int
+    # INTERNAL_FASTAPI_PORT: int
     
-    AUTH_BASE_URL: str
-    PRODUCT_BASE_URL: str
-    ORDER_BASE_URL: str
+    # AUTH_BASE_URL: str
+    # PRODUCT_BASE_URL: str
+    # ORDER_BASE_URL: str
             
     model_config = SettingsConfigDict(
         case_sensitive=False,
