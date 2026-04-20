30d28356e469c4abb9d7284fe151e4da1193bc6f
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2026-04-20 11:16:39 +0330

    push project

--- uv-assistant.py
+++ uv-assistant.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import os
 import re
 import shutil
 import subprocess
+import importlib.util
 from pathlib import Path
-from typing import Iterable, List, Set
+from typing import Iterable
 import json
-
 
 # Python 3.11+ has tomllib in stdlib. For 3.10- fallback to 'tomli' if installed.
 try:
@@ -19,35 +16,59 @@ except ModuleNotFoundError:  # pragma: no cover
     except ModuleNotFoundError:
         tomllib = None  # we'll guard its usage
 
+exclusive_packages = {
+    "fastapi",
+    "pydantic",
+    "pymongo",
+    "motor",
+}
+
+mirror = [
+    "--index",
+    # "https://mirror-pypi.runflare.com/simple",
+    # "https://package-mirror.liara.ir/repository/pypi",
+    "https://repo.hmirror.ir/python/simple",
+]
+
 base_env = {
     "UV_MANAGED": "false",
     "UV_NO_SYNC_VENV": "true",
-    # برای خروجی‌های یکنواخت یونیکدی:
     "PYTHONUTF8": "1",
 }
 
 def run_cmd(
-    cmd: List[str],
+    cmd: list[str],
     cwd: Path | None = None,
+    return_result: bool = False,
 ) -> str:
 
     env = os.environ.copy()
     env.update(base_env)
-
-    proc = subprocess.run(
-        cmd,
-        cwd=str(cwd) if cwd else None,
-        env=env,
-        capture_output=True,
-        text=True,
-    )
     
-    if proc.returncode != 0:
-        print(' '.join(cmd))
-        print(proc.stderr.strip())
-            
-    return proc.stdout
+    print()
+    print(' '.join(cmd))
+    print()
+    
+    if return_result:
 
+        proc = subprocess.run(
+            cmd,
+            cwd=str(cwd) if cwd else None,
+            env=env,
+            capture_output=True,
+            text=True,
+        )
+        
+        if proc.returncode != 0:
+            print(proc.stderr.strip())
+                
+        return proc.stdout
+    else:
+        subprocess.run(
+            cmd,
+            cwd=str(cwd) if cwd else None,
+            shell=True,
+        )
 
 def name_only(
     name: str,
@@ -63,7 +84,7 @@ def name_only(
 
 def parse_pyproject_dependencies(
     pyproject_path: Path,
-) -> Set[str]:
+) -> set[str]:
     
     if not pyproject_path.exists():
         return set()
@@ -77,11 +98,11 @@ def parse_pyproject_dependencies(
 
 def parse_requirements_names(
     req_path: Path,
-) -> Set[str]:
+) -> set[str]:
     
     if not req_path.exists():
         return set()
-    names: Set[str] = set()
+    names: set[str] = set()
     for line in req_path.read_text(encoding="utf-8").splitlines():
         line = line.strip()
         if not line or line.startswith("#") or line.startswith("-r "):
@@ -104,39 +125,90 @@ def ensure_uv_available() -> None:
     if shutil.which("uv") is None:
         raise RuntimeError("Uv is not installed.")
     
+def ensure_venv_available(
+    dir: Path,
+) -> None:
+    
+    venv = dir / ".venv"
+    
+    if venv.exists():
+        print("venv exists, Run 'uv sync'")
+        run_cmd(
+            [
+                "uv",
+                "sync",
+                *mirror,
+            ],
+        )
+    else:
+        print("venv not exists, Run 'uv venv'")
+        run_cmd(
+            cmd=[
+                "uv",
+                "venv"
+            ],
+            cwd=dir,
+        )
+        ensure_venv_available(dir)
+    
+def ensure_pyproject(
+    dir: Path,
+) -> None:
+
+    run_cmd(
+        cmd=[
+            "uv",
+            "init",
+            "--bare",  
+        ],
+        cwd=dir,
+    )
+    
+def check_requirements(
+    dir: Path,
+):
+    installed = importlib.util.find_spec("pipdeptree") is not None
+    ensure_pyproject(dir)
+    if not installed:
+        print("pipdeptree is not installed, Installing pipdeptree...")
+        run_cmd(
+            cmd=[
+                "uv",
+                "add",
+                "pipdeptree",
+                *mirror,
+                "--active",
+            ],
+            cwd=dir,
+        )
+    
 def get_top_packages(dir: Path):
     
     requirements = ""
         
     result = run_cmd(
-        cmd=["pipdeptree", "--json"],
+        cmd=[
+            "uv",
+            "run",
+            "pipdeptree",
+            "--json",
+        ],
         cwd=dir,
+        return_result=True,
     )
     data: list[dict] = json.loads(result)
-    
-    exc = {
-        "pydantic",
-        "pymongo",
-        "motor",
-    }
     
     deps = set()
     top_pkgs = set()
     top_pkgs_with_version = dict()
     for pkg in data:
-        
-        # if pkg["package"]["package_name"] == "PyYAML":
-        #     print(pkg)
-            
+                    
         top_pkgs.add(pkg["package"]["package_name"])
         top_pkgs_with_version[pkg["package"]["package_name"]] =  pkg["package"]["installed_version"]
         for depend in pkg.get("dependencies", []):
-            deps.add(depend["package_name"])
-            
-            # if depend["package_name"] == "PyYAML":
-            #     print(pkg)           
+            deps.add(depend["package_name"])        
         
-    top_pkgs = top_pkgs - (deps - exc)
+    top_pkgs = top_pkgs - (deps - exclusive_packages)
     top_pkgs = sorted(top_pkgs)
     for k, v in top_pkgs_with_version.items():
         for p in top_pkgs:
@@ -148,56 +220,20 @@ def get_top_packages(dir: Path):
 def process(
     dir: Path,
 ) -> None:
-
+        
     pyproject = dir / "pyproject.toml"
     requirements = dir / "requirements.txt"
     
-    pyproject_exists = pyproject.exists()
-
-    print(str(dir))
-
-    if not pyproject_exists:
-        run_cmd(
-            cmd=[
-                "uv",
-                "init",
-                "--bare",
-                str(dir),
-            ],
-            cwd=dir,
-        )
-    
-    # proc = run_cmd(
-    #     cmd=[
-    #         "uv",
-    #         "pip",
-    #         "freeze",
-    #     ],
-    #     cwd=dir,
-    # )
+    ensure_pyproject(dir)
         
     requirements.write_text(
         get_top_packages(dir),
         encoding="utf-8",
     )
 
-    # run_cmd(
-    #     cmd=[
-    #         "uv",
-    #         "add",
-    #         "-r",
-    #         "requirements.txt",
-    #         "--active",
-    #         # "--no-sync",
-    #         "--frozen",
-    #     ],
-    #     cwd=dir,
-    # )
-
     deps_set = parse_pyproject_dependencies(pyproject)
     req_names = parse_requirements_names(requirements)
     to_remove = sorted(deps_set - req_names)
-    # to_remove = False
 
     if to_remove:
         print(f"Pruning from pyproject.toml: {' '.join(to_remove)}")
@@ -219,6 +255,7 @@ def process(
             "add",
             "-r",
             "requirements.txt",
+            *mirror,
             "--active",
             # "--no-sync",
             "--frozen",
@@ -246,6 +283,8 @@ def remove_uv_lock(
 def main() -> None:
 
     ensure_uv_available()
+    ensure_venv_available(Path.cwd())
+    check_requirements(Path.cwd())
 
     paths_list = list(discover_dirs(Path.cwd()))
 
@@ -254,14 +293,15 @@ def main() -> None:
 
     for path in paths_list:
         try:
+            print(path)
             process(path)
             remove_uv_lock(path)
             print()
         except Exception as e:
-            raise SystemExit(f"Processing Error for {path.name}:\n{e}\n") from e
+            raise
+            # raise SystemExit(f"Processing Error for {path.name}:\n{e}\n") from e
     
     remove_uv_lock(Path.cwd())       
-
 
 if __name__ == "__main__":
     main()
